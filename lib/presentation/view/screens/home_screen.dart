491c7a73a0a26e3c5006c6cb39fd52a40bd8d48f
ahmedhossam1212 <pmedo1689@gmail.com>
2023-11-18 21:22:19 +0200

    finish home UI

--- lib/presentation/view/screens/home_screen.dart
+++ lib/presentation/view/screens/home_screen.dart
@@ -128,7 +128,87 @@ class HomeScreen extends StatelessWidget {
           Container(
             color: AppColors.grey,
             width: double.infinity,
+            height: context.height * 0.7,
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Image.asset(
+                    "assets/photos/tamas-pap.png",
+                    height: context.height * 0.4,
+                  ),
+                  SizedBox(
+                    height: context.height * 0.075,
+                  ),
+                  Center(
+                    child: Text(
+                      "If you want to adopt a pet",
+                      style: getBoldStyle(
+                          color: AppColors.darkBrown, fontSize: 25),
+                    ),
+                  ),
+                  SizedBox(
+                    height: context.height * 0.05,
+                  ),
+                  Center(
+                    child: CustomButton(
+                        textColor: AppColors.offWhite,
+                        backgroundColor: AppColors.darkBrown,
+                        lable: "Apply now",
+                        height: context.height * 0.07,
+                        width: context.width * 0.8,
+                        onTap: () {},
+                        isOutlined: false),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            width: double.infinity,
             height: context.height * 0.8,
+            color: AppColors.darkBrown,
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "assets/photos/richard.png",
+                    height: context.height * 0.45,
+                  ),
+                  Center(
+                    child: Text(
+                      "The pet loves care and always needs it",
+                      style:
+                          getBoldStyle(color: AppColors.offWhite, fontSize: 25),
+                    ),
+                  ),
+                  SizedBox(
+                    height: context.height * 0.01,
+                  ),
+                  Text(
+                    "Learn how to take care of your pet so that he loves you. Get some awareness articles about diseases and risks.",
+                    style: getRegularStyle(
+                        color: AppColors.offWhite, fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: context.height * 0.05,
+                  ),
+                  Center(
+                    child: CustomButton(
+                        textColor: AppColors.darkBrown,
+                        backgroundColor: AppColors.offWhite,
+                        lable: "Help Them",
+                        height: context.height * 0.07,
+                        width: context.width * 0.8,
+                        onTap: () {},
+                        isOutlined: false),
+                  ),
+                ],
+              ),
+            ),
           )
         ]),
       ),
